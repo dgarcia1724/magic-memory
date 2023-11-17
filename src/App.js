84f13c4eb3ec8918f0241417bdb85cd963932880
Danny Garcia <dgarcia1724@berkeley.edu>
2023-11-16 19:30:22 -0800

    68. fresh start

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   // shuffle cards
   const shuffleCards = () => {
@@ -36,6 +37,7 @@ function App() {
   useEffect(
     function () {
       if (choiceOne && choiceTwo) {
+        setDisabled(true);
         if (choiceOne.src === choiceTwo.src) {
           setCards((prevCards) => {
             return prevCards.map((card) => {
@@ -62,6 +64,7 @@ function App() {
     setChoiceOne(null);
     setChoiceTwo(null);
     setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
   };
 
   return (
@@ -76,6 +79,7 @@ function App() {
             card={card}
             handleChoice={handleChoice}
             flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
         ))}
       </div>
